3f8fa9ed2e49e8491e02ebdbe417796b01433fb7
Ymac VSCode <51804657+Klimrod89@users.noreply.github.com>
2024-02-11 08:27:10 -0500

    Created the console app

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+IConfigurationBuilder builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", false, true);
+IConfigurationRoot config = builder.Build();
+
+string mySetting = config["MySettings"]!;
+mySetting = config.GetValue<string>("MySettings")!;
+
+Console.WriteLine($"MySetting Value: {mySetting}");
+
+Console.ReadLine();
