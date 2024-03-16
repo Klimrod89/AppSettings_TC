475f1938660178b6318a744a20e216a5417b21d8
Ymac VSCode <51804657+Klimrod89@users.noreply.github.com>
2024-03-16 17:39:12 -0400

    Revision of what I have learned previously

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,9 +8,16 @@ IConfigurationRoot config = builder.Build();
 
 string mySetting = config["MySettings"]!;
 mySetting = config.GetValue<string>("MySettings")!;
+mySetting = (string)config.GetValue(typeof(string), "MySettings")!;
 string mySubSetting = config.GetValue<string>("MainSetting:SubSetting")!;
+
+string? mySubSubSetting = config["MainSetting:SubSection:SubSubSetting"];
+
+string? connectionString = config.GetConnectionString("Default");
 
 Console.WriteLine($"MySetting Value: {mySetting}");
 Console.WriteLine($"MySubSetting Value: {mySubSetting}");
+Console.WriteLine($"MySubSubSetting Value: {mySubSubSetting}");
+Console.WriteLine($"ConnectionString Value: {connectionString}");
 
 Console.ReadLine();
